df460087370e6732f703d3a26ea02dfc05dcbf27
Renato Andrade <renato.emanuel.andrades@gmail.com>
2022-05-02 16:39:24 -0300

    run lint

--- cypress/integration/const/urls.js
+++ cypress/integration/const/urls.js
@@ -2,9 +2,8 @@ const URL_CARD = '/cards';
 const URL_LIST = '/lists';
 const URL_MEMBER = '/members';
 
-
 export default {
-    URL_CARD,
-    URL_LIST,
-    URL_MEMBER,
-}
+  URL_CARD,
+  URL_LIST,
+  URL_MEMBER,
+};
